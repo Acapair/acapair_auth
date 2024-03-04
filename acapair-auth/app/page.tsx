30c19e6b13f3f03a9cc37f57931d7c13ae54e900
boduroglu <burakboduroglu0@gmail.com>
2024-03-04 12:01:40 +0300

    FEAT: Home Page Design

---
+++ acapair-auth/app/page.tsx
@@ -0,0 +1,37 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+export default function Home() {
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-gradient-to-r from-slate-500 to-slate-900">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          🎓 Acapair
+        </h1>
+        <p className="text-white text-lg">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        </p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Giriş Yap
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
+}
