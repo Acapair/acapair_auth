8014c09a4a8e36bca749a879eee9db149bdf66b8
boduroglu <burakboduroglu0@gmail.com>
2024-03-04 19:06:46 +0300

    feat: :lipstick: Register form design finished.

--- acapair-auth/schemas/index.ts
+++ acapair-auth/schemas/index.ts
@@ -8,3 +8,15 @@ export const LoginSchema = z.object({
     message: "Şifre alanı boş bırakılamaz.",
   }),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Lütfen geçerli bir e-posta adresi girin.",
+  }),
+  password: z.string().min(6, {
+    message: "Şifre alanı 6 karakterden uzun olmalıdır.",
+  }),
+  name: z.string().min(1, {
+    message: "Ad alanı boş bırakılamaz.",
+  }),
+});
