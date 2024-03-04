3d292de7f0c2611c72f3132f0fd2d99c127a8a29
boduroglu <burakboduroglu0@gmail.com>
2024-03-04 20:35:15 +0300

    feat: :card_file_box: User register process.

--- acapair-auth/actions/register.ts
+++ acapair-auth/actions/register.ts
@@ -1,7 +1,9 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -10,5 +12,29 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Geçersiz alanlar!" };
   }
 
-  return { success: "E-posta gönderildi." };
+  const { email, password, name } = validatedFields.data;
+
+  // TODO - Pepper the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const existingUser = await db.user.findUnique({
+    where: { email },
+  });
+
+  if (existingUser) {
+    return { error: "Bu e-posta adresi zaten kullanılıyor." };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+    },
+  });
+
+  // TODO: Send email verification
+
+  return { success: "Kullanıcı başarılı bir şekilde oluşturuldu." };
 };
