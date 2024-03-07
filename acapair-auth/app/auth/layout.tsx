39ac5c2e7d6045ca9cf73f1beb213b819d02f322
boduroglu <burakboduroglu0@gmail.com>
2024-03-07 12:42:20 +0300

    feat: :zap: Image optimization.

--- acapair-auth/app/auth/layout.tsx
+++ acapair-auth/app/auth/layout.tsx
@@ -1,11 +1,26 @@
 import Image from "next/image";
 import Logo from "@/assets/pictures/navbar-logo.webp";
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Acapair | Hoş geldiniz!",
+  description: "Educational Stream Platform Login Page",
+};
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full flex justify-center bg-gradient-to-r from-slate-500 to-slate-900">
       <nav className="flex w-full shadow-lg fixed">
-        <Image src={Logo} alt="logo" width={180} height={180} />
+        <Image
+          src={Logo}
+          alt="acapair-logo"
+          width={180}
+          height={180}
+          sizes="180px"
+          decoding="async"
+          data-nimg="1"
+          style={{ color: "transparent" }}
+          priority
+        />
       </nav>
       <div className="flex items-center">{children}</div>
     </div>
