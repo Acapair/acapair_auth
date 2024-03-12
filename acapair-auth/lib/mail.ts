10b45af59c4710de56af4b81edc9c08dcedae189
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2024-03-12 13:44:56 +0300

    feat: :art: Email engine changed.

--- acapair-auth/lib/mail.ts
+++ acapair-auth/lib/mail.ts
@@ -1,13 +1,27 @@
-import { Resend } from "resend";
+const nodemailer = require("nodemailer");
 
-const resend = new Resend(process.env.API_KEY);
+const transporter = nodemailer.createTransport({
+  host: "mail.erciyes.edu.tr",
+  port: 587,
+  secure: false, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: "1030521090@erciyes.edu.tr",
+    pass: "Burak*1905",
+  },
+});
 
+// async..await is not allowed in global scope, must use a wrapper
 export const sendVertificationEmail = async (email: string, token: string) => {
   const confirmLink = `http://localhost:3000/auth/new-vertification?token=${token}`;
-  await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: email,
-    subject: "Please confirm your email",
-    html: `<p>Click <a href="${confirmLink}">here</a> to confirm your email</p>`,
+
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '"Acapair 🎓" <1030521090@erciyes.edu.tr>', // sender address
+    to: email, // list of receivers
+    subject: "Lütfen e-posta adresinizi doğrulayın", // Subject line
+    text: "EPosta Doğrulama", // plain text body
+    html: `<p> Lütfen e-posta adresinizi<a href="${confirmLink}">doğrulayın</a></p>`, // html body
   });
+
+  console.log("Message sent: %s", info.messageId);
 };
