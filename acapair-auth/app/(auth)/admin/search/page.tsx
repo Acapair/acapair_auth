f40ad89dcaf359801f624cdbe66f771593794eb8
boduroglu <burakboduroglu0@gmail.com>
2024-04-13 19:41:50 +0300

    fix: :bug: Hydration error

--- acapair-auth/app/(auth)/admin/search/page.tsx
+++ acapair-auth/app/(auth)/admin/search/page.tsx
@@ -5,13 +5,13 @@ const AdminPage = async () => {
   const role = await currentRole();
   if (role !== "ADMIN") {
     return (
-      <div className="text-white text-xl text-bold mt-5">
+      <div className="text-bold mt-5 text-xl text-white">
         Erişim izniniz yok.
       </div>
     );
   }
   return (
-    <div className="flex-row items-center justify-center bg-white p-10 rounded-xl w-[680px]">
+    <div className="w-[680px] flex-row items-center justify-center rounded-xl bg-white p-10">
       <SearchBar />
     </div>
   );
